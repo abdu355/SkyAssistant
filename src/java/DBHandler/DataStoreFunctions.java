7362dea993936d3220c0526191f13553b75bd92a
abdu355 <abdu_sah@hotmail.com>
2016-04-03 00:55:46 +0400

    v2.2
    *reverted back to DataStore

--- src/java/DBHandler/DataStoreFunctions.java
+++ src/java/DBHandler/DataStoreFunctions.java
@@ -23,27 +23,33 @@ import javax.servlet.http.HttpServlet;
  */
 public class DataStoreFunctions {
 
-    Cluster cluster;
-    Bucket bucket;
+    //Cluster cluster;
+    //Bucket bucket;
     DatastoreService ds;
     int uniqueId;
 
     public DataStoreFunctions() {
         int uniqueId = 0;
         ds = DatastoreServiceFactory.getDatastoreService();
-        cluster = CouchbaseCluster.create();
-        bucket = cluster.openBucket();
+        //cluster = CouchbaseCluster.create();
+        //bucket = cluster.openBucket();
     }
 
     public void createUser(String... params) {
-        //String uniqueID = UUID.randomUUID().toString();
-        JsonObject user = JsonObject.empty()
-                .put("fullname", params[0])
-                .put("email", params[1])
-                .put("password", params[2])
-                .put("DOB", params[3]);
-        JsonDocument doc = JsonDocument.create(params[0], user);
-        JsonDocument response = bucket.upsert(doc);
+        String uniqueID = UUID.randomUUID().toString();
+//        JsonObject user = JsonObject.empty()
+//                .put("fullname", params[0])
+//                .put("email", params[1])
+//                .put("password", params[2])
+//                .put("DOB", params[3]);
+//        JsonDocument doc = JsonDocument.create(params[0], user);
+//        JsonDocument response = bucket.upsert(doc);
+        Entity e = new Entity("User", uniqueID);
+        e.setProperty("FullName", params[0]);
+        e.setProperty("Password", params[1]);
+        e.setProperty("Email", params[2]);
+        e.setProperty("DOB", params[3]);
+        ds.put(e);
     }
 
     private int getUniqueId() {
